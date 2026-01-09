951fc250270422f2fc18ddad2304235e7ff28b83
Skye Waetje <wswaetje@gmail.com>
2026-01-09 03:14:50 -0800

    idfk what I did?

---
+++ CMakeFiles/Eukariot.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Eukariot.dir/link.d"
+  "CMakeFiles/Eukariot.dir/main.cpp.o"
+  "CMakeFiles/Eukariot.dir/main.cpp.o.d"
+  "Eukariot"
+  "Eukariot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Eukariot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
